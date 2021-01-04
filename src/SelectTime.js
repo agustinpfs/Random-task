d4069c323b96e03196711972d6d9d14b2cbf09c9
Agustin <agu@cursosit.com>
2021-01-04 13:15:58 -0300

    edit SelectTime component, change state(onChange)

--- src/SelectTime.js
+++ src/SelectTime.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import TimePickers from './TimePickers';
+import React, { useState } from "react";
 
 function SelectTime() {
+
+    const [newTime, setNewTime] = useState('00:00');
+
+    const updateNewTime = e => setNewTime(e.target.value); //cambio al estado con lo que tipea el user
+
+
+
     return (
         <div>
             <h1>¿Cuánto tiempo trabajarás hoy?</h1>
-            <TimePickers />
+            <div>
+                <input
+                    type="text"
+                    // value={newTime}
+                    onChange={updateNewTime}
+                    placeholder="add hours"
+                />
+                {/* <button onClick={createNewTime}>
+                    Add
+            </button> */}
+                <h1>{newTime}</h1>
+            </div>
 
         </div>
     )
